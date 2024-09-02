278a50c6b05387caaf2a734de4ae49940d11889a
Samuel Forrest <samueljforrest@outlook.com>
2024-09-02 11:45:39 +0100

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,18 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+
+      }}
+    >
+      <Text style={{
+        fontFamily: "outfit-bold",
+        fontSize:40,
+      }}>
+        Edit app/index.jsx to edit this screen.</Text>
+    </View>
+  );
+}
